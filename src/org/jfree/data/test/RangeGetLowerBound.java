53f7339a5fca0508b74b0f21c819e99b094cf44b
ajtoyoda <ajtoyoda@ucalgary.ca>
2015-02-09 15:48:26 -0700

    Added in commenting and better method names for functions

--- src/org/jfree/data/test/RangeGetLowerBound.java
+++ src/org/jfree/data/test/RangeGetLowerBound.java
@@ -7,33 +7,44 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+/***
+ * Perform blackbox testing on Range.getLowerBound()
+ * @author Taylor
+ *
+ */
 public class RangeGetLowerBound {
 
 	@Before
 	public void setUp() {
-		//
+		//No set up is necessary because each test case needs a new range
 	}
 
 	@After
 	public void tearDown() {
+		//Ensure it is reset for safety
 		range = null;
 	}
 
 	@Test
-	public void testGetLowerBound1() {
+	public void testGetLowerBoundNegativeLowerBound() {
 		range = new Range(-1,1);
 		assertEquals(-1, range.getLowerBound(), 0.00001d);
 	}
 	@Test
-	public void testGetLowerBound2() {
+	public void testGetLowerBoundLowerBoundIsUpperBound() {
 		range = new Range(0,0);
 		assertEquals(0, range.getLowerBound(), 0.00001d);
 	}
 	@Test
-	public void testGetLowerBound3() {
+	public void testGetLowerBoundLargeLowerBound() {
 		range = new Range(9999999, 999999999);
 		assertEquals(9999999, range.getLowerBound(), 0.00001d);
 	}
-	
+
+	@Test 
+	public void testGetLowerBoundPositiveLowerBound(){
+		range = new Range(2,5);
+		assertEquals(0,range.getLowerBound(), 0.00001d);
+	}
 	private Range range;
 }
