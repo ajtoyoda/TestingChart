36e0f3073b4e26e1329c71ea1e36671c12de9301
Spencer <prazakspencer@gmail.com>
2015-02-09 15:26:20 -0700

    Column Total and 2D Array tested

---
+++ src/org/jfree/data/test/DataUtilitiesCreateNumberArray2DTest.java
@@ -0,0 +1,106 @@
+package org.jfree.data.test;
+
+import org.jfree.data.DataUtilities;
+import org.junit.*;
+import junit.framework.TestCase;
+import java.security.InvalidParameterException;
+
+// Tests the createNumberArray2D() method of the DataUtilities class.
+//
+public class DataUtilitiesCreateNumberArray2DTest extends TestCase {
+	private double[][] m_array; // Array of doubles to convert.
+	
+	// Helper method to initialize m_array to null.
+	//
+	private void createNullArray()
+	{
+		m_array = null;	
+	}
+	
+	// Helper method to initialize m_array with a single value.
+	//
+	private void create1DArray()
+	{
+		double[] holder = { 1, 1.11, -1.52, 0 };
+		m_array = new double[][] { holder };
+	}
+	
+	// Helper method to initialize m_array with many different
+	// values.
+	//
+	private void createArray()
+	{
+		double[] holder = { 1.0, 1.11, -1.52, 0.0 };
+		double[] holder2 = { 5.0, -2.0, 9.0, -12.0 };
+		m_array = new double[][] { holder, holder2};
+	}
+	
+	// Verifies createNumberArray2D throws an exception for an invalid array.
+	//
+	@Test (expected = InvalidParameterException.class)
+	public void testThrowsInvalidParameterExceptionForNullArray()
+	{
+		createNullArray();
+		DataUtilities.createNumberArray2D(m_array);
+	}
+	
+	// Verifies createNumberArray2D() copies the values of a single number array
+	// to a new array of the same length.
+	//
+	@Test
+	public void testCopiesSingleValueArrayLength()
+	{
+		create1DArray();
+		Number[][] actual = DataUtilities.createNumberArray2D(m_array);
+		
+		assertEquals("Length of arrays should be equal.",
+				m_array.length, actual.length);
+	}
+	
+	// Verifies createNumberArray2D() duplicates the contents of a single-
+	// value array.
+	//
+	@Test
+	public void testCopiesSingleValueArrayData()
+	{
+		create1DArray();
+		Number[][] actual = DataUtilities.createNumberArray2D(m_array);
+		
+		assertEquals("Data of arrays should be equal.",
+				m_array[0][0], actual[0][0].doubleValue(), 0.0000001);
+	}
+	
+	// Verifies createNumberArray2D() duplicates the length of a multi-value
+	// array.
+	//
+	@Test
+	public void testCopiesArrayLength()
+	{
+		createArray();
+		Number[][] actual = DataUtilities.createNumberArray2D(m_array);
+		
+		assertEquals("Length of arrays should be equal.",
+				m_array.length, actual.length);
+	}
+	
+	// Verifies createNumberArray2D() duplicates the data of a multi-value
+	// array.
+	//
+	@Test
+	public void testCopiesArrayData()
+	{
+		createArray();
+		Number[][] actual = DataUtilities.createNumberArray2D(m_array);
+		
+		assertEquals("Array content must be equal", m_array[0][0], actual[0][0].doubleValue());
+		assertEquals("Array content must be equal", m_array[0][1], actual[0][1].doubleValue());
+		assertEquals("Array content must be equal", m_array[0][2], actual[0][2].doubleValue());
+		assertEquals("Array content must be equal", m_array[0][3], actual[0][3].doubleValue());
+		assertEquals("Array content must be equal", m_array[1][0], actual[1][0].doubleValue());
+		assertEquals("Array content must be equal", m_array[1][1], actual[1][1].doubleValue());
+		assertEquals("Array content must be equal", m_array[1][2], actual[1][2].doubleValue());
+		assertEquals("Array content must be equal", m_array[1][3], actual[1][3].doubleValue());
+		
+		
+	}
+}
