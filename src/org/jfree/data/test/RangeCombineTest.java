3873fbd93eedc7283bea56095d2fea002b2a95ad
ajtoyoda <ajtoyoda@ucalgary.ca>
2015-02-19 16:22:29 -0700

    Made RangeCombine tests and implemented clover

--- src/org/jfree/data/test/RangeCombineTest.java
+++ src/org/jfree/data/test/RangeCombineTest.java
@@ -16,10 +16,42 @@ public class RangeCombineTest extends TestCase {
 	@After
 	public void tearDown() {
 		exampleRange = null;
+		exampleRange2 = null;
 	}
 
+	public void testBothNull(){
+		assertTrue(Range.combine(null, null)==null);
+	}
+	public void testFirstNull(){
+		exampleRange = new Range(1,1);
+		assertEquals(Range.combine(null, exampleRange), exampleRange);
+	}
 	
-	private Range exampleRange;
+	public void testSecondNull(){
+		exampleRange = new Range(1,1);
+		assertEquals(Range.combine(exampleRange, null), exampleRange);
+	}
+	
+	public void testFirstRangeHigherThanSecond(){
+		exampleRange= new Range(1,2);
+		exampleRange2 = new Range(2,3);
+		assertEquals(Range.combine(exampleRange, exampleRange2),new Range(1,3));
+	}
+	public void testSecondRangeHigherThanFirst(){
+		exampleRange= new Range(1,2);
+		exampleRange2 = new Range(2,3);
+		assertEquals(Range.combine(exampleRange2, exampleRange),new Range(1,3));
+	}
+	public void testCombineSameRange(){
+		exampleRange = new Range(1,2);
+		assertEquals(Range.combine(exampleRange, exampleRange), exampleRange);
+	}
+	public void testGapInRange(){
+		exampleRange = new Range(1,2);
+		exampleRange2 = new Range(3,4);
+		assertEquals(Range.combine(exampleRange, exampleRange2), new Range(1,4));
+	}
+	private Range exampleRange, exampleRange2;
 
 }
 
